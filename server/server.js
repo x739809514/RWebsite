5613942bfc7ada195bb2caa03d229328da4219f9
Russell Xin <1612650023@qq.com>
2024-09-17 17:33:19 +0100

    1

--- server/server.js
+++ server/server.js
@@ -170,6 +170,6 @@ app.get('/articles', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'Images')));
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
