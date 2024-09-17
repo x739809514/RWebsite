285be239d492e3f6ae1ff6660ff8e6adac55c11c
Russell Xin <1612650023@qq.com>
2024-09-17 18:48:01 +0100

    修改

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage');
+});
+
 // 设置静态文件目录
 
 const articlesDir = path.join(__dirname, '../articles');
